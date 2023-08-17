fa2cf646868ceed3521538014b93d466df9dc4a7
Jaime <2519260008jjordanc@gmail.com>
2023-08-16 18:06:08 -0600

    corsback

--- middlewares/checkAuth.js
+++ middlewares/checkAuth.js
@@ -13,7 +13,7 @@ const checkAuth = async (req, res, next) => {
          return next();
 
        } catch (error) {
-            return  res.status(404).json({msg: 'Hubo un error'});
+            return  res.status(404).json({msg: 'Hubo un error'}); 
        }
     }
 

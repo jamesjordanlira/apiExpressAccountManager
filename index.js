60ce790815cc19ef936b4b3ef5e5bf4cb18097d2
Jaime <2519260008jjordanc@gmail.com>
2023-08-16 20:03:09 -0600

    cambio en index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import cors from 'cors';
 import usuarioRoutes from './routes/usuarioRoutes.js';
 import cuentaRoutes from './routes/cuentaRoutes.js';
 
-
 const app = express();
 
 app.use(express.json());
@@ -17,23 +16,21 @@ conectarDB();
 
 const whitelist = [process.env.frontend_uri];
 const corOptions = {
-    
-    origin: function(origin, callback) {
-        if(whitelist.includes(origin)){
-            callback(null, true);
-        }else{
-            callback(new Error('Error de Cors'));
-        }
+  origin: function (origin, callback) {
+    if (!origin || whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Error de CORS'));
     }
+  },
 };
 
-
 app.use(cors(corOptions));
-
-
 
 app.use('/api/usuarios', usuarioRoutes);
 app.use('/api/cuentas', cuentaRoutes);
 
-app.listen(process.env.PORT);
-console.log(`Servidor en el puerto ${process.env.PORT}`); 
+const PORT = process.env.PORT || 80;
+app.listen(PORT, () => {
+  console.log(`Servidor en el puerto ${PORT}`);
+});
